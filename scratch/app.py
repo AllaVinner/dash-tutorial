06172cb113e7d886eefd1394918f377e1e8c8568
allavinner <joel.w.ottosson@gmail.com>
2023-08-29 08:10:58 +0200

    Initialized scratch app

---
+++ scratch/app.py
@@ -0,0 +1,31 @@
+from dash import Dash, dcc, html, Input, Output, callback
+
+app = Dash(__name__)
+
+my_input = dcc.Input(value='initial value', type='text')
+my_output = html.Div()
+my_output2 = html.Div()
+
+app.layout = html.Div([
+    html.H6("Change the value in the text box to see callbacks in action!"),
+    html.Div([
+        "Input: ",
+        my_input
+    ]),
+    html.Br(),
+    my_output,
+    my_output2,
+    html.Div('Done')
+])
+
+
+@callback(
+    Output(my_output, 'children'),
+    Input(my_input, 'value')
+)
+def update_output_div(input_value):
+    return f'Output: {input_value}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
